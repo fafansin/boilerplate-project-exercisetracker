3292abec8c40c952f27b9fdf852a36f0fa406883
laviel <fafansin@gmail.com>
2024-07-04 10:00:53 +0300

    feat: handle create user post request and get users request

--- index.js
+++ index.js
@@ -2,25 +2,36 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config()
 
-const User = require('./models/User');
+const User = require('./models/user');
 
 // Mongo Atlas Connection
 mongoose.connect(process.env.CONN_STRING, {dbName:'exercisetracker'})
   .then(() => console.log('Mongodb Connection Success!'))
-  .catch((e) => console.error('Mongodb Connection Failed!', error))
+  .catch((e) => console.error('Mongodb Connection Failed!', e))
 
 
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use(express.static('public'))
 app.get('/', async(req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', async (req, res) => {
+  const {username} = req.body;
+  const ref = await User.create({username:username});
+  res.json({username:ref.username, _id:ref.id})
+})
 
-
+app.get('/api/users', async (req, res) => {
+  const ref = await User.find({}, 'username');
+  res.json(ref);
+})
 
 
 
