0d6648b05786a5fd9a8b2cc0b7a61a66f78b2c23
laviel <fafansin@gmail.com>
2024-07-04 20:02:33 +0300

    fix:handle invalid id

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.get('/', async(req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+
 app.post('/api/users', async (req, res) => {
   const {username} = req.body;
   const ref = await User.create({username:username});
@@ -34,13 +35,14 @@ app.get('/api/users', async (req, res) => {
   res.json(ref);
 })
 
+
 app.post('/api/users/:id/exercises', async (req, res)=> {
   const { description, duration, date} = req.body;
   const { id } = req.params;
   
   const user = await User.findById(id);
   if(!user){
-    return res.json({error:"User nor found"});
+    return res.json({error:"User not found"});
   }
   // Form exercise obj
   const exercise = await Exercise.create({
@@ -49,7 +51,6 @@ app.post('/api/users/:id/exercises', async (req, res)=> {
     date:date ? new Date(date) : new Date(),
     person:user._id
   })
-  
   
   res.json({
     username:user.username, 
@@ -64,33 +65,38 @@ app.get('/api/users/:id/logs', async (req, res) => {
   const { from, to, limit } = req.query;
   const {id} = req.params;
   
-  const user = await User.findById(id);
+  try{
+    const user = await User.findById(id);
 
-  const filter = { person:id }
-  const all = await Exercise.find(filter, 'description').populate('person', 'username');
-  
-  if(from && to){
-    filter.date = {$gte:new Date(from), $lte: new Date(to)}
-  }else if(from){
-    filter.date = {$gte: new Date(from)}
-  }else if(to){
-    filter.date = {$lte: new Date(to)}
+    const filter = { person:id }
+
+    if(from && to){
+      filter.date = {$gte:new Date(from), $lte: new Date(to)}
+    }else if(from){
+      filter.date = {$gte: new Date(from)}
+    }else if(to){
+      filter.date = {$lte: new Date(to)}
+    }
+
+
+    const exercises = await Exercise.find(filter).limit(limit ? limit : 0);
+    const logs = exercises.map(item => {
+      return {description:item.description, duration:item.duration, date:item.date.toDateString()}
+    })
+
+    res.json({
+      _id:id,
+      username:user.username,
+      count:logs.length,
+      ...(from && {from:new Date(from).toDateString()}),
+      ...(to && {to:new Date(to).toDateString()}),
+      log:logs
+    });
+  }catch(e){
+    console.error('ERROR', e.message);
+    res.json({error:'Query error'})
   }
   
-  const exercises = await Exercise.find(filter).limit(limit ? limit : 0);
-
-  const logs = exercises.map(item => {
-    return {description:item.description, duration:item.duration, date:item.date.toDateString()}
-  })
-  
-  res.json({
-    _id:id,
-    username:user.username,
-    count:logs.length,
-    ...(from && {from:new Date(from).toDateString()}),
-    ...(to && {to:new Date(to).toDateString()}),
-    log:logs
-  });
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
